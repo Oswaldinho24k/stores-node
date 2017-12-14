bc13b88a14d0e6ad1bca7544588c750f74c6b766
Oswaldinho <oswalfut_96@hotmail.com>
2017-12-13 20:56:28 -0600

    lol

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store')
+const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next){
+      const isPhoto = file.mimetype.startsWith('image/');
+      if(isPhoto){
+          next(null, true);
+      }else{
+          next({message:'no porno please!!'}, false);
+      }
+  }
+};
 
 
 exports.myMiddleware = (req, res, next) => {
@@ -16,6 +31,22 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
     res.render('editStore', {title:'Add Store'})
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next)=>{
+    if(!req.file){
+        next();
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    next();
 };
 
 exports.createStore = async (req, res) => {
@@ -43,10 +74,18 @@ exports.editStore = async (req, res) => {
 
 
 exports.updateStore = async (req, res) => {
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({_id:req.params.id}, req.body, {
         new: true, 
         runValidators:true, 
     }).exec();
     req.flash('success', `Successfully updated store ${store.name}`);
     res.redirect(`/stores/${store._id}/edit`);
+};
+
+exports.getStoreBySlug = async(req, res, next)=>{
+
+    const store = await Store.findOne({slug:req.params.slug});
+    if(!store) return next();
+    res.render('store', {store, title:store.name})
 };
--- models/Store.js
+++ models/Store.js
@@ -8,21 +8,49 @@ const storeSchema = new mongoose.Schema({
         trim: true,
         required: 'Please enter a Store Name'
     },
-    store :String,
+    slug :String,
     description:{
         type:String,
         tream:true
     },
-    tags: [String]
+    tags: [String],
+    created:{
+        type:Date,
+        default: Date.now
+    },
+    location:{
+        type:{
+            type:String,
+            default:'Point'
+        },
+        coordinates:[{
+                type:Number,
+                required:'please dame tu pinche ubicacion'
+            }],
+        address:{
+            type:'String',
+            required:'agora tu dirección'
+        }
+    },
+    photo:String
 });
 
 
-storeSchema.pre('save', function(next){
+storeSchema.pre('save', async function(next){
     if(!this.isModified('name')){
         next();
         return;
     }
+
     this.slug = slug(this.name);
+    //find others that named the same
+    const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`,'i');
+
+    const storeWithSlug = await this.constructor.find({slug:slugRegEx});
+
+    if(storeWithSlug.length){
+        this.slug = `${this.slug}-${storeWithSlug.length + 1}`;
+    }
     next();
 });
 
